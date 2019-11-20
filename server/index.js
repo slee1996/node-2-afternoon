749cec2c691663f7e49726f887e32e5caa0024ff
Spencer Lee <spencerlee96@gmail.com>
2019-11-20 13:29:42 -0700

    chatting up a storm now

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const mc = require('./controllers/messages_controller')
+
+//App variable and use params
+const app = express()
+
+app.use(express.json())
+app.use(express.static(__dirname + '/../public/build'))
+
+//Port assignment and listener
+const port = 3001
+
+app.listen(port, () => {
+    console.log(`server blazing on port ${port}`)
+})
+
+//Endpoints
+const messagesBaseUrl = '/api/messages'
+app.post(messagesBaseUrl, mc.create)
+app.get(messagesBaseUrl, mc.read)
+app.put(`${messagesBaseUrl}/:id`, mc.update)
+app.delete(`${messagesBaseUrl}/:id`, mc.delete)
